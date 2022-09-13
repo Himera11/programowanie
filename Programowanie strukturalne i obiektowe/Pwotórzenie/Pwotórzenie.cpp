c03c655e4fc58e38af2c3b4dd4ef1b6061a39316
Himera11 <damian.manecki@gmail.com>
2022-09-13 09:52:01 +0200

    koniec powórki

--- Programowanie strukturalne i obiektowe/Pwotórzenie/Pwotórzenie.cpp
+++ Programowanie strukturalne i obiektowe/Pwotórzenie/Pwotórzenie.cpp
@@ -8,6 +8,8 @@ void menu()
 	cout << "1. pole kwadratu\n";
 	cout << "2. pole trojkata\n";
 	cout << "3. Historia bezdomnego\n";
+
+	cout << "0. zamknij program\n";
 } 
 
 int option()
@@ -21,9 +23,9 @@ int option()
 void Polekwadratu()
 {
 	float a;
-	cout << "poda pan bok biednemu: \n";
+	cout << "poda pan bok biednemu: ";
 	cin >> a;
-	cout << "Dzieki ci panie a twe pole wynosi: " << a * a;
+	cout << "Dzieki ci panie a twe pole wynosi: " << a * a << endl;
 }
 void Poletrojkata()
 {
@@ -33,24 +35,24 @@ void Poletrojkata()
 	cin >> a;
 	cout << "bok panie tez daj bo ja biedny taki bardzo tu \n";
 	cin >> b;
-	cout << "dzieki ci panie niech bogowie beda ci przychylni " << 0.5 * a * b;
+	cout << "dzieki ci panie niech bogowie beda ci przychylni " << 0.5 * a * b<< endl;
 }
 void Historia()
 {
 	system("cls");
-	cout << "no mialem se dom i juz nie mam co ja mam powiedziec z domu mnie wykopali bo za duzo kasy na lola wydalem to wiecie, okolo 40 kola wyszlo na lola a no i bronza mialem oczywiscie";
+	cout << "no mialem se dom i juz nie mam co ja mam powiedziec z domu mnie wykopali bo za duzo kasy na lola wydalem to wiecie, okolo 40 kola wyszlo na lola a no i bronza mialem oczywiscie\n";
 }
 void koniecHistori()
 {
 	system("cls");
 
-	cout << "smieciem jestes ja smutny teraz.";
+	cout << "smieciem jestes ja smutny teraz.\n";
 }
 void Historiabezdomnego()
 {
 	system("cls");
 
-	cout << "Chcesz us³yszec moja historie:   wybierz liczbe   1_tak/2_nie";
+	cout << "Chcesz us³yszec moja historie:   wybierz liczbe   1_tak/2_nie\n";
 	int wybor;
 	cin >> wybor;
 	
@@ -78,17 +80,26 @@ void end(int selectedOption)
 	case 3:
 		Historiabezdomnego();
 		break;
+	case 0:
+		return;
 	default:
 		cout << "JOE BIDEN";
+	}
+	if (selectedOption != 0 )
+	{
+		system("pause");
 	}
 }
 
 void mainprogram()
 {
-	menu();
-	
-	int selected = option();
+	int selected;
+	do
+	{
+		menu();
+	 selected = option();
 	end(selected);
+	} while (selected != 0);
 }
 
 
