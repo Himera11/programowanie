81bd42673819614e2d8929b32c8792329ce33731
Himera11 <damian.manecki@gmail.com>
2022-09-30 08:14:14 +0200

    s

--- Programowanie strukturalne i obiektowe/struktury/struktury.cpp
+++ Programowanie strukturalne i obiektowe/struktury/struktury.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
 using namespace std;
 
+struct Point
+{
+	int x;
+	int y;
+};
+
+double calculatedDistance(int a, int b)
+{
+	double distance = sqrt(a * a + b * b);
+	return distance;
+}
 void coordinateTestV1()
 {
-	int x, y;
 	
+	//Point point;
+	int x, y;
 	cout << "podaj x\n";
 	cin >> x;
 	cout << "podaj y\n";
 	cin >> y;
-	double d = sqrt(x*x + y*y);
-	
-	cout << "odleglosc od (0,0) wynosi " << d;
+	//double d = sqrt(x*x + y*y);
+	double distance = calculatedDistance(x, y);
+	cout << "odleglosc od (0,0) wynosi " << distance;
+}
+
+void coordinateTestV2()
+{
+	Point point;
+	cout << "podaj x\n";
+	cin >> point.x;
+	cout << "podaj y\n";
+	cin >> point.y;
+	//double d = sqrt(x*x + y*y);
+	double distance = calculatedDistance(point.x, point.y);
+	cout << "odleglosc od (0,0) wynosi " << distance;
 }
 
 int main()
 {
-	coordinateTestV1();
+	//coordinateTestV1();
+	coordinateTestV2();
+
 }
